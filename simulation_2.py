d509755f7c512c926a40a53b89092e11ee559246
Jase Rost <jaserost@yahoo.com>
2018-12-09 18:20:07 -0700

    Included reversability

--- simulation_2.py
+++ simulation_2.py
@@ -23,8 +23,8 @@ if __name__ == '__main__':
     #create routers and routing tables for connected clients (subnets)
     encap_tbl_D = {'H1': '1', 'H2': '2', 'H3': '3'}    # table used to encapsulate network packets into MPLS frames
     #Still need to be able to have source, to send to new route
-    frwd_tbl_D = {'1': ['11', 'H1', 2], '2':['12','H2',3]} #, '3':['4','RB',0], '4':['5','RC',1]}     # table used to forward MPLS frames
-    decap_tbl_D = {}#'99':'H3','98':'H2'}    # table used to decapsulate network packets from MPLS frames
+    frwd_tbl_D = {'1': ['11', 'H1', 2], '2':['12','H2',3], '98': ['99', 'H3', 1]} #, '3':['4','RB',0], '4':['5','RC',1]}     # table used to forward MPLS frames
+    decap_tbl_D = {'99':'H3'}    # table used to decapsulate network packets from MPLS frames
     router_a = Router(name='RA', 
                               intf_capacity_L=[500,500, 500, 500],
                               encap_tbl_D = encap_tbl_D,
@@ -36,7 +36,7 @@ if __name__ == '__main__':
     #H1 here is where you're sending it, and 11 is on what interface I think
     encap_tbl_D = {}
     #12 here is what it's coming in as, then 11 is what you're putting it in as next, destination, and the interface
-    frwd_tbl_D = {'11': ['21', 'H1', 1]}# '7':['6','H1',1]}
+    frwd_tbl_D = {'11': ['21', 'H1', 1], '97': ['98', 'H3', 0]}# '7':['6','H1',1]}
     #If it's 11, decapsulate, and send it to H2
     decap_tbl_D = {}
     router_b = Router(name='RB', 
@@ -59,9 +59,9 @@ if __name__ == '__main__':
     object_L.append(router_c)
 
     #create routers and routing tables for connected clients (subnets)
-    encap_tbl_D = {}#'H1': '1','H2': '2'}    # table used to encapsulate network packets into MPLS frames
+    encap_tbl_D = {'H3': '96'}#'H1': '1','H2': '2'}    # table used to encapsulate network packets into MPLS frames
     #Still need to be able to have source, to send to new route
-    frwd_tbl_D = {'21': ['31', 'H1', 2], '22': ['32', 'H2', 2]}#'1': ['7', 'H1', 2], '2':['8','H2',1], '6':['97','H3',0]}     # table used to forward MPLS frames
+    frwd_tbl_D = {'21': ['31', 'H1', 2], '22': ['32', 'H2', 2], '96': ['97', 'H3', 0]}#'1': ['7', 'H1', 2], '2':['8','H2',1], '6':['97','H3',0]}     # table used to forward MPLS frames
     decap_tbl_D = {'31':'H1', '32':'H2'}    # table used to decapsulate network packets from MPLS frames
     router_d = Router(name='RD',
                               intf_capacity_L=[500,500, 500],
@@ -102,7 +102,7 @@ if __name__ == '__main__':
     host_1.udt_send('H1', 'MESSAGE_%d_FROM_H1' % 0, 0)
     #host_1.udt_send('H3', 'MESSAGE_%d_FROM_H1' % 1, 1)
     host_2.udt_send('H2', 'MESSAGE_%d_FROM_H2' % 0, 0)
-    #host_3.udt_send('H1', 'MESSAGE_%d_FROM_H3' % 0, 1)
+    host_3.udt_send('H3', 'MESSAGE_%d_FROM_H3' % 0, 1)
 
     #create some send events    
     for i in range(3):
@@ -111,7 +111,11 @@ if __name__ == '__main__':
 
     for i in range(3):
         priority = i%2
-        host_1.udt_send('H2', 'MESSAGE_%d_FROM_H2' % i, priority)
+        host_2.udt_send('H2', 'MESSAGE_%d_FROM_H2' % i, priority)
+
+    for i in range(3):
+        priority = i%2
+        host_3.udt_send('H3', 'MESSAGE_%d_FROM_H3' % i, priority)
         
     #give the network sufficient time to transfer all packets before quitting
     sleep(simulation_time)
